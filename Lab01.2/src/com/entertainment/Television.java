ce59f0989cea5014482d602c8b6a47d8c8194f02
Jakira Dunson <jakiradunson@gmail.com>
2024-06-06 16:06:26 -0500

    push

--- Lab01.2/src/com/entertainment/Television.java
+++ Lab01.2/src/com/entertainment/Television.java
@@ -51,40 +51,52 @@ public class Television {
     }
 
     @Override
-    public int hashCode() {
-        // this is a poor hash function, because it easily yields "hash collisions"
-        // a "hash collision" is when "different" objects happen to have the same hashcode by dumb luck.
-        // return getBrand().length() + getVolume();
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Television that = (Television) o;
+        return getVolume() == that.getVolume() && Objects.equals(getBrand(), that.getBrand());
+    }
 
-        // this is a "scientifically correct" hash function, i.e.,
-        // it minimizes the probability of hash collisions
-        //this combines the hash codes from brand and volume and combines it as one
+    @Override
+    public int hashCode() {
         return Objects.hash(getBrand(), getVolume());
     }
-
-    // equals()
-    @Override
-    public boolean equals(Object obj) {
-        boolean result = false;
-        // check that 'obj' is really referring to a Television object
-        //if (obj instanceof Television) {This is a IS -A  check
-        if (this.getClass() == obj.getClass()) { //are we the exact same type
-            //If obj is an instance of Television, we cast obj to a Television type and assign it to a new variable other.
-            // This allows us to access Television-specific methods and fields.
-            // downcast 'obj' to more specific reference type Television, for getName(), getAge()
-            Television other = (Television) obj;
-
-            // do the checks: business equality is defined as brand, volume
-            //the current object is the 'this' the other obj is the 'other'
-            //Objects.equals(this.getBrand(), other.getBrand()): This checks if the brand of both televisions are equal.
-            // The Objects.equals method is used for a null-safe check (i.e., it handles cases where either or both brands might be null).
-            //this.getVolume() == other.getVolume(): This checks if the volume of both televisions are equal.
-            // Since volume is likely a primitive type (e.g., int), it cannot be null.
-            result = Objects.equals(this.getBrand(), other.getBrand()) &&  // null-safe check
-                    this.getVolume() == other.getVolume();                // primitives can't be null
-        }
-        return result;
-    }
+    //    @Override
+//    public int hashCode() {
+//        // this is a poor hash function, because it easily yields "hash collisions"
+//        // a "hash collision" is when "different" objects happen to have the same hashcode by dumb luck.
+//        // return getBrand().length() + getVolume();
+//
+//        // this is a "scientifically correct" hash function, i.e.,
+//        // it minimizes the probability of hash collisions
+//        //this combines the hash codes from brand and volume and combines it as one
+//        return Objects.hash(getBrand(), getVolume());
+//    }
+//
+//    // equals()
+//    @Override
+//    public boolean equals(Object obj) {
+//        boolean result = false;
+//        // check that 'obj' is really referring to a Television object
+//        //if (obj instanceof Television) {This is a IS -A  check
+//        if (this.getClass() == obj.getClass()) { //are we the exact same type
+//            //If obj is an instance of Television, we cast obj to a Television type and assign it to a new variable other.
+//            // This allows us to access Television-specific methods and fields.
+//            // downcast 'obj' to more specific reference type Television, for getName(), getAge()
+//            Television other = (Television) obj;
+//
+//            // do the checks: business equality is defined as brand, volume
+//            //the current object is the 'this' the other obj is the 'other'
+//            //Objects.equals(this.getBrand(), other.getBrand()): This checks if the brand of both televisions are equal.
+//            // The Objects.equals method is used for a null-safe check (i.e., it handles cases where either or both brands might be null).
+//            //this.getVolume() == other.getVolume(): This checks if the volume of both televisions are equal.
+//            // Since volume is likely a primitive type (e.g., int), it cannot be null.
+//            result = Objects.equals(this.getBrand(), other.getBrand()) &&  // null-safe check
+//                    this.getVolume() == other.getVolume();                // primitives can't be null
+//        }
+//        return result;
+//    }
 
     // toString()
     @Override

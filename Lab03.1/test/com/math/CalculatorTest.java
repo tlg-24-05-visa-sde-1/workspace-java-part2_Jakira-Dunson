7692945d05d110599e3106e31199f15f5ee1b1a3
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 16:41:43 -0500

    Notes Added!

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,26 +9,44 @@
 package com.math;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+    //business object(s) under test - called a "fixture"
+    private Calculator calculator;
+
+    //set-up method
+    @Before
+    public void setUp()  {
+        System.out.println("Set- Up");
+        calculator = new Calculator();
+    }
+    @After
+    public void tearDown() { //used as a clean-up method. Not used a- lot, useful for files, used for things that open/ close
+        System.out.println("Clean- Up");
+
+    }
 
     @Test
     public void testAdd() {
-        Calculator calc = new Calculator();
-        assertEquals(5, calc.add(1, 4));  // expected, actual
+        System.out.println("Test Add");
+        assertEquals(5, calculator.add(1, 4));  // expected, actual
     }
 
     @Test
     public void testDivide() {
-        Calculator calcu = new Calculator();
-        assertEquals(2.5, calcu.divide(5, 2), .001); //expected, actual, delta 
+        System.out.println("Test Divide");
+        assertEquals(2.5, calculator.divide(5, 2), .001); //expected, actual, delta
     }
 
     @Test
     public void testEven(){
-        Calculator calcul = new Calculator();
-        assertTrue(calcul.isEven(10));
+        System.out.println("Test Even");
+        assertTrue(calculator.isEven(10));
+        assertFalse(calculator.isEven(13));
 
     }
 }

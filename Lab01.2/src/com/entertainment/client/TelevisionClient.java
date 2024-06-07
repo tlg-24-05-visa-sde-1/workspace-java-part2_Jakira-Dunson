4c908418d58df46375612b00f950a4fb999ba839
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 12:10:24 -0500

    edititng television class

--- Lab01.2/src/com/entertainment/client/TelevisionClient.java
+++ Lab01.2/src/com/entertainment/client/TelevisionClient.java
@@ -4,6 +4,7 @@ import com.entertainment.Television;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 class TelevisionClient {
 
@@ -11,19 +12,24 @@ class TelevisionClient {
 
         // examine == and the equals() method
         Television tvA = new Television("Sony", 50);
-        Television tvB = new Television("LG", 50);
-
-        System.out.println(tvA.hashCode());
-        System.out.println(tvB.hashCode());
+        Television tvB = new Television("Sony", 50);
+        Television tvC = new Television("Samsung", 52);
+        Television tvD = new Television("LG", 12);
 
         System.out.println("tvA == tvB: " + (tvA == tvB));
         System.out.println("tvA.equals(tvB): " + tvA.equals(tvB));
         System.out.println();
 
-        Set<Television> tvs = new HashSet<>();
+        Set<Television> tvs = new TreeSet<>();
         tvs.add(tvA);
         tvs.add(tvB);
+        tvs.add(tvC);
+        tvs.add(tvD);
 
         System.out.println("The size of the set is: " + tvs.size());
+
+        for (Television tv : tvs) {
+            System.out.println(tv);
+        }
     }
 }

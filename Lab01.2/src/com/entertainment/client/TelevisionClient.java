c3ba7d5ac9f471a783137b13db7363f6881589d6
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 17:53:19 -0500

    edits

--- Lab01.2/src/com/entertainment/client/TelevisionClient.java
+++ Lab01.2/src/com/entertainment/client/TelevisionClient.java
@@ -3,6 +3,7 @@ package com.entertainment.client;
 import com.entertainment.Television;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -13,8 +14,8 @@ class TelevisionClient {
         // examine == and the equals() method
         Television tvA = new Television("Sony", 50);
         Television tvB = new Television("Sony", 50);
-        Television tvC = new Television("Samsung", 52);
-        Television tvD = new Television("LG", 12);
+        Television tvC = new Television("Sony", 52);
+        Television tvD = new Television("Sony", 12);
 
         System.out.println("tvA == tvB: " + (tvA == tvB));
         System.out.println("tvA.equals(tvB): " + tvA.equals(tvB));
@@ -24,10 +25,11 @@ class TelevisionClient {
         /*
         * Television did not have natural order Treeset would not be happy and exception is trown
         *
-        *
+        * HashSet has no order
+        * LinkedHashset has order of things
         * */
 
-        Set<Television> tvs = new HashSet<>();
+        Set<Television> tvs = new TreeSet<>();
         tvs.add(tvA);
         tvs.add(tvB);
         tvs.add(tvC);

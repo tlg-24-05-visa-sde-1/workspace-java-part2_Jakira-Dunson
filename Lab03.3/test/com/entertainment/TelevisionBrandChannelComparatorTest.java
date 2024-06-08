24da6babd69774b8b23ac23e576b8dc6d039f061
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 21:01:48 -0500

    television test

--- Lab03.3/test/com/entertainment/TelevisionBrandChannelComparatorTest.java
+++ Lab03.3/test/com/entertainment/TelevisionBrandChannelComparatorTest.java
@@ -1,8 +1,53 @@
 package com.entertainment;
 
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TelevisionBrandChannelComparatorTest {
-    //come back to it later
+    private Television television;
+    private Television television2;
+    private TelevisionBrandChannelComparator comparator;
 
+    @Before
+    public void setUp() {
+        television = new Television("Samsung");  // channel 3 by default
+        television2 = new Television("Samsung");  // channel 3 by default
+        comparator = new TelevisionBrandChannelComparator();
+    }
+
+    @Test
+    public void compare_shouldReturnZero_whenSameBrand_sameChannel() {
+        assertEquals(0, comparator.compare(television, television2));
+    }
+
+    @Test
+    public void compare_shouldReturnNegativeNumber_when1stBrandLessThan2ndBrand() {
+        television.setBrand("A_lessThan");
+        television2.setBrand("B_greaterThan");
+        assertTrue(comparator.compare(television, television2) < 0);
+    }
+
+    @Test
+    public void compare_shouldReturnPositiveNumber_when1stBrandGreaterThan2ndBrand() {
+        television.setBrand("B_greaterThan");
+        television2.setBrand("A_lessThan");
+        assertTrue(comparator.compare(television, television2) > 0);
+    }
+
+    @Test
+    public void compare_shouldReturnNegativeNumber_whenSameBrand_1stChannelLessThan2ndChannel()
+            throws InvalidChannelException {
+        television.changeChannel(1);
+        television2.changeChannel(2);
+        assertTrue(comparator.compare(television, television2) < 0);
+    }
+
+    @Test
+    public void compare_shouldReturnPositiveNumber_whenSameBrand_1stChannelGreaterThan2ndChannel()
+            throws InvalidChannelException {
+        television.changeChannel(2);
+        television2.changeChannel(1);
+        assertTrue(comparator.compare(television, television2) > 0);
+    }
 }
--- Lab03.3/test/com/entertainment/TelevisionChannelComparatorTest.java
+++ Lab03.3/test/com/entertainment/TelevisionChannelComparatorTest.java
@@ -1,8 +1,39 @@
 package com.entertainment;
 
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TelevisionChannelComparatorTest {
-    //come back to it later
+    private Television television;
+    private Television television2;
+    private TelevisionChannelComparator comparator;
 
+    @Before
+    public void setUp() {
+        television = new Television();  // channel 3 by default
+        television2 = new Television();  // channel 3 by default
+
+        // also need to create the Comparator<Television> object
+        comparator = new TelevisionChannelComparator();
+    }
+
+    @Test
+    public void compare_shouldReturnZero_whenSameChannel() {
+        assertEquals(0, comparator.compare(television, television2));
+    }
+
+    @Test
+    public void compare_shouldReturnNegativeNumber_when1stChannelLessThan2ndChannel()
+            throws InvalidChannelException {
+        television.changeChannel(2);
+        assertTrue(comparator.compare(television, television2) < 0);
+    }
+
+    @Test
+    public void compare_shouldReturnPositiveNumber_when1stChannelGreaterThan2ndChannel()
+            throws InvalidChannelException {
+        television.changeChannel(4);
+        assertTrue(comparator.compare(television, television2) > 0);
+    }
 }

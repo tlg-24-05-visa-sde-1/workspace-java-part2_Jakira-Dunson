074365dbea848a5664b06b09864c3256c0207264
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 20:17:38 -0500

    employee test

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -14,12 +14,20 @@ public class EmployeeTest {
 
     @Before
     public void setUp() throws Exception {
-        employee = new SalariedEmployee("Nova", Date.valueOf("2017-07-18"));
-        employee2 = new SalariedEmployee("Nova", Date.valueOf("2017-07-18"));
+        employee = new SalariedEmployee("Nova", Date.valueOf("2017-01-01"));
+        employee2 = new SalariedEmployee("Nova", Date.valueOf("2017-01-01"));
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_sameName_differentHireDate() {
+       employee2.setHireDate(Date.valueOf("2016-11-22"));
+       assertNotEquals(employee, employee2);
+       assertFalse(employee.equals(employee2));
     }
 
     @Test
     public void equals_shouldReturnTrue_allPropertiesSame() {
         assertEquals(employee, employee2); //this does an .equals() check
+        assertTrue(employee.equals(employee2)); //alternative assertion
     }
 }

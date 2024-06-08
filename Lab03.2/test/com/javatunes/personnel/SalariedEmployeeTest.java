874f53767ced52bb99e1db33637f905bcd763fd8
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 19:20:14 -0500

    salary employee class test

---
+++ Lab03.2/test/com/javatunes/personnel/SalariedEmployeeTest.java
@@ -0,0 +1,12 @@
+package com.javatunes.personnel;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SalariedEmployeeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}

7fee125deeedf4c8d57997abcfc27ccef2c25d0c
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 20:43:57 -0500

    television test

---
+++ Lab03.3/test/com/entertainment/TelevisionTest.java
@@ -0,0 +1,50 @@
+package com.entertainment;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+* BVT:
+* volume
+* -1, 0 and 100, 101
+* channel
+* 0, 1 and 999, 1000
+*/
+
+
+public class TelevisionTest {
+    private Television television;
+
+    @Before
+    public void setUp()  {
+        television = new Television();
+    }
+
+    @Test(expected = InvalidChannelException.class)
+    public void changeChannel_shouldThrowIllegalArgumentException_invalidInput_lowerBound(){
+        television.changeChannel(0);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void setVolume_shouldThrowIllegalArgumentException_invalidInput_upperBound(){
+        television.setVolume(101);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setVolume_shouldThrowIllegalArgumentException_invalidInput_lowerBound(){
+        television.setVolume(-1);
+    }
+
+    @Test
+    public void setVolume_shouldStoreValue_validInput_lowerBound() {
+        television.setVolume(0);
+        assertEquals(0, television.getVolume());
+    }
+
+    @Test
+    public void setVolume_shouldStoreValue_validInput_upperBound() {
+        television.setVolume(100);
+        assertEquals(100, television.getVolume());
+    }
+}

8df0da35831354815f820afc14c21870426fd5bd
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 22:18:45 -0500

    collections

--- Lab04.1-Extra/src/com/javatunes/catalog/InMemoryCatalog.java
+++ Lab04.1-Extra/src/com/javatunes/catalog/InMemoryCatalog.java
@@ -9,10 +9,8 @@
 
 package com.javatunes.catalog;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 // OF COURSE THIS CLASS DOESN'T COMPILE
 // Your first job is to fulfill the contract that this class has signed.
@@ -20,24 +18,24 @@ public class InMemoryCatalog implements Catalog {
 
     private final List<MusicItem> catalogData = new ArrayList<>(List.of(
             /* id    title                        artist                       releaseDate  price  musicCategory */
-            new MusicItem(1L,  "Diva",                      "Annie Lennox",              "1992-01-04", 13.99, MusicCategory.POP),
-            new MusicItem(2L,  "Dream of the Blue Turtles", "Sting",                     "1985-02-05", 14.99, MusicCategory.POP),
-            new MusicItem(3L,  "Trouble is...",             "Kenny Wayne Shepherd Band", "1997-08-08", 14.99, MusicCategory.BLUES),
-            new MusicItem(4L,  "Lie to Me",                 "Jonny Lang",                "1997-08-26", 17.97, MusicCategory.BLUES),
-            new MusicItem(5L,  "Little Earthquakes",        "Tori Amos",                 "1992-01-18", 14.99, MusicCategory.ALTERNATIVE),
-            new MusicItem(6L,  "Seal",                      "Seal",                      "1991-08-18", 17.97, MusicCategory.POP),
-            new MusicItem(7L,  "Ian Moore",                 "Ian Moore",                 "1993-12-05",  9.97, MusicCategory.CLASSICAL),
-            new MusicItem(8L,  "So Much for the Afterglow", "Everclear",                 "1997-01-19", 13.99, MusicCategory.ROCK),
-            new MusicItem(9L,  "Surfacing",                 "Sarah McLachlan",           "1997-12-04", 17.97, MusicCategory.ALTERNATIVE),
-            new MusicItem(10L, "Hysteria",                  "Def Leppard",               "1987-06-20", 17.97, MusicCategory.ROCK),
-            new MusicItem(11L, "A Life of Saturdays",       "Dexter Freebish",           "2000-12-06", 16.97, MusicCategory.RAP),
-            new MusicItem(12L, "Human Clay",                "Creed",                     "1999-10-21", 18.97, MusicCategory.ROCK),
-            new MusicItem(13L, "My, I'm Large",             "Bobs",                      "1987-02-20", 11.97, MusicCategory.COUNTRY),
-            new MusicItem(14L, "So",                        "Peter Gabriel",             "1986-10-03", 17.97, MusicCategory.POP),
-            new MusicItem(15L, "Big Ones",                  "Aerosmith",                 "1994-05-08", 18.97, MusicCategory.ROCK),
-            new MusicItem(16L, "90125",                     "Yes",                       "1983-10-16", 11.97, MusicCategory.ROCK),
-            new MusicItem(17L, "1984",                      "Van Halen",                 "1984-08-19", 11.97, MusicCategory.ROCK),
-            new MusicItem(18L, "Escape",                    "Journey",                   "1981-02-25", 11.97, MusicCategory.CLASSIC_ROCK))
+            new MusicItem(1L, "Diva", "Annie Lennox", "1992-01-04", 13.99, MusicCategory.POP),
+            new MusicItem(2L, "Dream of the Blue Turtles", "Sting", "1985-02-05", 14.99, MusicCategory.POP),
+            new MusicItem(3L, "Trouble is...", "Kenny Wayne Shepherd Band", "1997-08-08", 14.99, MusicCategory.BLUES),
+            new MusicItem(4L, "Lie to Me", "Jonny Lang", "1997-08-26", 17.97, MusicCategory.BLUES),
+            new MusicItem(5L, "Little Earthquakes", "Tori Amos", "1992-01-18", 14.99, MusicCategory.ALTERNATIVE),
+            new MusicItem(6L, "Seal", "Seal", "1991-08-18", 17.97, MusicCategory.POP),
+            new MusicItem(7L, "Ian Moore", "Ian Moore", "1993-12-05", 9.97, MusicCategory.CLASSICAL),
+            new MusicItem(8L, "So Much for the Afterglow", "Everclear", "1997-01-19", 13.99, MusicCategory.ROCK),
+            new MusicItem(9L, "Surfacing", "Sarah McLachlan", "1997-12-04", 17.97, MusicCategory.ALTERNATIVE),
+            new MusicItem(10L, "Hysteria", "Def Leppard", "1987-06-20", 17.97, MusicCategory.ROCK),
+            new MusicItem(11L, "A Life of Saturdays", "Dexter Freebish", "2000-12-06", 16.97, MusicCategory.RAP),
+            new MusicItem(12L, "Human Clay", "Creed", "1999-10-21", 18.97, MusicCategory.ROCK),
+            new MusicItem(13L, "My, I'm Large", "Bobs", "1987-02-20", 11.97, MusicCategory.COUNTRY),
+            new MusicItem(14L, "So", "Peter Gabriel", "1986-10-03", 17.97, MusicCategory.POP),
+            new MusicItem(15L, "Big Ones", "Aerosmith", "1994-05-08", 18.97, MusicCategory.ROCK),
+            new MusicItem(16L, "90125", "Yes", "1983-10-16", 11.97, MusicCategory.ROCK),
+            new MusicItem(17L, "1984", "Van Halen", "1984-08-19", 11.97, MusicCategory.ROCK),
+            new MusicItem(18L, "Escape", "Journey", "1981-02-25", 11.97, MusicCategory.CLASSIC_ROCK))
     );
 
     // CONTRACT METHODS - these are specified in the Catalog interface and implemented here
@@ -58,7 +56,6 @@ public class InMemoryCatalog implements Catalog {
         }
         return item;
     }
-
     /**
      * Returns a collection of items that match the supplied keyword.
      * This is basically a search method.
@@ -70,9 +67,10 @@ public class InMemoryCatalog implements Catalog {
      */
     @Override
     public Collection<MusicItem> findByKeyword(String keyword) {
-        return List.of();
-    }
+        Collection<MusicItem> result = new ArrayList<>();
 
+        return result;
+    }
     /**
      * Returns a collection of items that are of the supplied genre (category).
      */
@@ -81,15 +79,20 @@ public class InMemoryCatalog implements Catalog {
         // return value
         Collection<MusicItem> items = new ArrayList<>();
 
-        for (MusicItem currentItem : catalogData) {
-            if (currentItem.getMusicCategory() == category) { // == is okay for enum types
-                items.add(currentItem);
+//        for (MusicItem currentItem : catalogData) {
+//            if (currentItem.getMusicCategory() == category) { // == is okay for enum types
+//                items.add(currentItem);
+//            }
+//        }
+//
+//        return items;
+        for (MusicItem item : catalogData) {
+            if (item.getMusicCategory() == category) {  // == is okay for enum types
+                items.add(item);
             }
         }
-
         return items;
     }
-
     /**
      * Size of the catalog.
      */
@@ -98,19 +101,16 @@ public class InMemoryCatalog implements Catalog {
 
         return catalogData.size();
     }
-
     /**
      * Read-only view of the entire catalog.
      * See java.util.Collections class (an all-static utility class) for help here.
      */
     @Override
     public Collection<MusicItem> getAll() {
-        Collection<MusicItem> readOnlyCatalog = Collections.unmodifiableList(catalogData);
-
-        return readOnlyCatalog;
+        //Collection<MusicItem> readOnlyCatalog = Collections.unmodifiableList(catalogData);
+        // return readOnlyCatalog;
+        return Collections.unmodifiableCollection(catalogData);
     }
-
-
     /**
      * After you've satisfied your contractual obligations above, do these additional tasks.
      *
@@ -145,11 +145,8 @@ public class InMemoryCatalog implements Catalog {
                 items.add(currentItem);
             }
         }
-
         return items;
     }
-
-
     /**
      * TASK: find all "rock" items whose price is less than or equal to the specified price.
      */
@@ -163,27 +160,20 @@ public class InMemoryCatalog implements Catalog {
                 items.add(currentItem);
             }
         }
-
         return items;
     }
-
-
     /**
      * TASK: how many items of the specified genre (MusicCategory) do we sell?
      */
     public int genreCount(MusicCategory category) {
         Collection<MusicItem> items = new ArrayList<>();
-
         for (MusicItem currentItem : catalogData) {
             if (currentItem.getMusicCategory().equals(category)) {
                 items.add(currentItem);
             }
-
         }
         return items.size();
     }
-
-
     /**
      * TASK: determine average price of our low-cost, extensive catalog of music.
      */
@@ -196,8 +186,6 @@ public class InMemoryCatalog implements Catalog {
 
         return (result / catalogData.size());
     }
-
-
     /**
      * TASK: find the cheapest item with the specified genre (MusicCategory).
      */
@@ -208,8 +196,7 @@ public class InMemoryCatalog implements Catalog {
             if (currentItem.getMusicCategory().equals(category)) {
                 if (cheapestAlbum == null) {
                     cheapestAlbum = currentItem;
-                }
-                else {
+                } else {
                     if (currentItem.getPrice() < cheapestAlbum.getPrice()) {
                         cheapestAlbum = currentItem;
                     }
@@ -219,8 +206,6 @@ public class InMemoryCatalog implements Catalog {
 
         return cheapestAlbum;
     }
-
-
     /**
      * TASK: find the average price of items in the specified genre (MusicCategory).
      */
@@ -239,42 +224,83 @@ public class InMemoryCatalog implements Catalog {
 
         return sum / sameGenre.size();
     }
-
-
     /**
      * TASK: are all items priced at least $10?
      * This is a yes/no answer.
      */
-
-
+    public boolean areItemsAtLeastTenDollars() {
+        for (MusicItem currentItem : catalogData) {
+            if (currentItem.getPrice() < 10.0) {
+                return false;
+            }
+        }
+        return true;
+    }
     /**
      * TASK: do we sell any items with the specified genre (MusicCategory)?
      * Another yes/no answer.
      */
-
-
+    public boolean itemsSoldWithSpecificGenre(MusicCategory category) {
+        for (MusicItem currentItem : catalogData) {
+            if (currentItem.getMusicCategory() == category) {
+                return true;
+            }
+        }
+        return false;
+    }
     /**
      * TASK: find the titles of all "pop" items, sorted by natural order.
      * Just the titles!
      */
-
-
+    public Collection<String> findPopTitlesSorted() {
+        List<String> popTitles = new ArrayList<>();
+        for (MusicItem item : catalogData) {
+            if (item.getMusicCategory() == MusicCategory.POP) {
+                popTitles.add(item.getTitle());
+            }
+        }
+        Collections.sort(popTitles);
+        return popTitles;
+    }
     /**
      * TASK: find all items released in the 80s whose price is less than or equal to the specified price.
      */
-
-
+    public Collection<MusicItem> findItemsFrom80s(double price) {
+        Collection<MusicItem> items = new ArrayList<>();
+        for (MusicItem item : catalogData) {
+            String yearString = item.getReleaseDate().substring(0, 4);
+            int year = Integer.parseInt(yearString);
+            if (year >= 1980 && year < 1990 && item.getPrice() <= price) {
+                items.add(item);
+            }
+        }
+        return items;
+    }
     /**
      * TASK: return a map whose keys are all the genres (categories), and each key's associated value
      * is a collection of items in that genre.  If there is a genre that we don't currently
      * sell, that key's associated value should be an empty collection, not null.
      */
+    public Map<MusicCategory, Collection<MusicItem>> getItemsByCategory() {
+        Map<MusicCategory, Collection<MusicItem>> categoryMap = new EnumMap<>(MusicCategory.class);
 
+        // Initialize the map with empty collections for each category
+        for (MusicCategory category : MusicCategory.values()) {
+            categoryMap.put(category, new ArrayList<>());
+        }
+
+        // Fill the map with items
+        for (MusicItem item : catalogData) {
+            categoryMap.get(item.getMusicCategory()).add(item);
+        }
+
+        return categoryMap;
+    }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder(getClass().getSimpleName() + ": \n");
-        for (MusicItem item: catalogData) {
+        for (MusicItem item : catalogData) {
             builder.append(item).append("\n");
         }
         return builder.toString();

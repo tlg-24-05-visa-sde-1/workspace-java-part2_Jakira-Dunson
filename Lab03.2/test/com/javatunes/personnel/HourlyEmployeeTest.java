6b7f43d0b1ad083b641f5c198232aa91531a8120
Jakira Dunson <jakiradunson@gmail.com>
2024-06-07 19:47:36 -0500

    test

--- Lab03.2/test/com/javatunes/personnel/HourlyEmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/HourlyEmployeeTest.java
@@ -1,12 +1,22 @@
 package com.javatunes.personnel;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Date;
 
 import static org.junit.Assert.*;
 
 public class HourlyEmployeeTest {
+    private HourlyEmployee hourlyEmployee;
 
     @Before
     public void setUp()  {
+        hourlyEmployee = new HourlyEmployee("Nora", Date.valueOf("2010-1-10"),25.0,35.0);
+    }
+
+    @Test
+    public void testPayTaxes() {
+        assertEquals(218.75, hourlyEmployee.payTaxes(), 0.001);
     }
 }
--- Lab03.2/test/com/javatunes/personnel/SalariedEmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/SalariedEmployeeTest.java
@@ -4,23 +4,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Date;
-import java.time.LocalDate;
-import java.time.Month;
 
 import static org.junit.Assert.*;
 
 public class SalariedEmployeeTest {
 
     //business object(s) under test
-    private SalariedEmployee salEmp;
+    private SalariedEmployee salariedEmployee;
 
     @Before
     public void setUp()  {
-        salEmp = new SalariedEmployee("Jakira", Date.valueOf("2024-05-07"), 2500.0);
+        salariedEmployee = new SalariedEmployee("Jakira", Date.valueOf("2024-05-07"), 2500.0);
     }
 
     @Test
     public void testPayTaxes() {
-        assertEquals(750.0, salEmp.payTaxes(), .001);
+        assertEquals(750.0, salariedEmployee.payTaxes(), .001);
     }
 }

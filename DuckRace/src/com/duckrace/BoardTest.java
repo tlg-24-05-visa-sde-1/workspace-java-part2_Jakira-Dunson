93ecfea81bf6939defd2b3ca5f89d663f87d27e0
Jakira Dunson <jakiradunson@gmail.com>
2024-06-08 20:28:57 -0500

    duck race

--- DuckRace/src/com/duckrace/BoardTest.java
+++ DuckRace/src/com/duckrace/BoardTest.java
@@ -3,6 +3,13 @@ package com.duckrace;
 public class BoardTest {
     public static void main(String[] args) {
         Board board = new Board();
-        board.dumpStudentIdMap();
+        //board.dumpStudentIdMap();
+
+        board.updateBoard(2,Reward.DEBIT_CARD);
+        board.updateBoard(3,Reward.PRIZES);
+        board.updateBoard(4,Reward.DEBIT_CARD);
+        board.updateBoard(9,Reward.PRIZES);
+
+        board.dumpRacerMap();
     }
 }

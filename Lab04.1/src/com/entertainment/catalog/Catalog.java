804427ad20d50eaa2c1bfbd51926b730a4ed0dd7
Jakira Dunson <jakiradunson@gmail.com>
2024-06-08 21:26:50 -0500

    edits

--- Lab04.1/src/com/entertainment/catalog/Catalog.java
+++ Lab04.1/src/com/entertainment/catalog/Catalog.java
@@ -24,11 +24,20 @@ public class Catalog {
     }
 
     /**
-     * Searches catalog by brand, and returns a collection of matching Televisions.
-     * A no-matches result should be an empty collection (not null).
+     * Searches catalog by one or more brands, and returns a map with an entry (row)
+     * for each brand supplied,
+     * with a corresponding collection of matching Televisions for that brand.
+     * A no-brands-passed result should be an empty map (not null).
      */
     public static Collection<Television> findByBrand(String brand) {
-        return null;
+        Collection <Television> result = new ArrayList<>();
+
+        for (Television tv : catalog) {
+            if (tv.getBrand().equals(brand)) {
+                result.add(tv);
+            }
+        }
+        return result;
     }
 
     /**
@@ -37,22 +46,27 @@ public class Catalog {
      * A no-brands-passed result should be an empty map (not null).
      */
     public static Map<String,Collection<Television>> findByBrands(String... brands) {
-        return null;
-    }
+        Map<String, Collection<Television>> result = new HashMap<>();
 
+        for (String brand : brands) {
+            Collection<Television> television = findByBrand(brand);
+            result.put(brand, television);
+        }
+        return result;
+    }
     /**
      * Returns entire catalog.
      * NOTE: returning a direct reference to it has consequences!
      * A client can manipulate it, since it has a direct reference to it.
      * Sometimes this is okay, but not here.
-     * 
+     *
      * TODO: change this to return a read-only view of the catalog.
-     * 
+     *
      * You should explore the Javadoc for the java.util.Collections *class*.
      *  This is an all-static utility class, not the java.util.Collection interface.
      */
     public static Collection<Television> getInventory() {
-        return catalog;
+        return Collections.unmodifiableCollection(catalog);
     }
 
     /*

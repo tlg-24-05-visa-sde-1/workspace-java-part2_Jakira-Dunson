57e1939e4eecedb0240da3affd60f93df98a64c6
Jakira Dunson <jakiradunson@gmail.com>
2024-06-10 15:25:45 -0500

    order

--- Lab04.2/test/com/javatunes/product/OrderTest.java
+++ Lab04.2/test/com/javatunes/product/OrderTest.java
@@ -40,6 +40,14 @@ public class OrderTest {
      */
     @Test
     public void testProcessCart() {
+        Order order1 = new Order("order-1");
+        order1.processCart(productCart);
+
+        Order order2 = new Order("order-2");
+        order1.processCart(musicCart);
+
+        Order order3 = new Order("order-3");
+        order1.processCart(mediaCart);
 
     }
 }

dc8dea54a0bc2157b21229b786d1d03366ddeadf
Jakira Dunson <jakiradunson@gmail.com>
2024-06-11 14:52:51 -0500

    test

--- Lab05.3/src/com/javatunes/billing/TaxCalculatorFactory.java
+++ Lab05.3/src/com/javatunes/billing/TaxCalculatorFactory.java
@@ -1,4 +1,21 @@
 package com.javatunes.billing;
 
 public class TaxCalculatorFactory {
+
+    public static TaxCalculator getTaxCalculator(Location location) {
+        TaxCalculator calculator = null;
+
+        switch(location) {
+            case ONLINE:
+                calculator = new OnlineTax();
+                break;
+            case USA:
+                calculator = new USATax();
+                break;
+            case EUROPE:
+                calculator = new EuropeTax();
+                break;
+        }
+        return calculator;
+    }
 }
